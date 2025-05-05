95a3d65900cf63f7da44766d49f8215a2839af08
Sammy Krem <skrem@mit.edu>
2025-05-05 00:34:22 -0400

    test

--- shrinkrayheist/shrinkrayheist/state_machine.py
+++ shrinkrayheist/shrinkrayheist/state_machine.py
@@ -17,7 +17,7 @@ class StateMachine(Node):
         self.person_sub = self.create_subscription(Bool, '/shoe_detected', self.person_callback, 10)
         self.traffic_light_sub = self.create_subscription(Bool, '/red_light_detected', self.traffic_light_callback, 10)
         self.path_planned = self.create_subscription(Bool, '/path_planned', self.path_planned_callback, 10)
-        self.drive_pub = self.create_publisher(AckermannDriveStamped, "/vesc/low_level/input/safety", 10)
+        self.drive_pub = self.create_publisher(AckermannDriveStamped, "/vesc/low_level/input/navigation", 10)
         # Publisher to path planner (example, could be more complex in reality)
         self.goal_pub = self.create_publisher(PoseStamped, '/goal_pose', 10)
 
@@ -43,11 +43,8 @@ class StateMachine(Node):
     def person_callback(self, msg: Bool): #not necessary if using safety controller
         self.person_detected = msg.data
         if msg.data:
-            self.get_logger().info(f"Person detected: {msg.data}")
-            
-            
+            self.get_logger().info(f"Person detected: {msg.data}")            
 
- 
     def traffic_light_callback(self, msg: Bool):
         self.red_light_detected = msg.data
         if msg.data:
@@ -71,7 +68,8 @@ class StateMachine(Node):
                 self.get_logger().info("In WAIT state!")
                 drive_msg.drive.speed = 0.0  
         else:
-            drive_msg.drive.speed = 1.0
+            drive_msg.drive.speed = 0.0  
+            # drive_msg.drive.speed = 1.0
             self.drive_pub.publish(drive_msg)
 
 
